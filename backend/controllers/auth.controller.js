314e802e410f74dcef0765cbe35097eeb483bcfa
AnshulGupta <anshulgupta2028@gmail.com>
2024-12-04 04:17:49 +0530

    push

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
 
+// SIGN UP
 const createUser = async (req, res) => {
   try {
     const { username, email, password, role } = req.body;
@@ -16,16 +18,53 @@ const createUser = async (req, res) => {
         res: "User with this email id already exist !!",
       });
     }
+    // Store hash password in your  DB.
+    var salt = bcrypt.genSaltSync(10);
+    var hashPassword = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({ username, email, password, role });
+    const newUser = new User({ username, email, password: hashPassword, role });
     await newUser.save();
     res.send({
       status: true,
       res: "User created Successfully !!",
     });
-  } catch (error) {}
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// SIGN IN
+const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.send({
+        status: false,
+        res: "Please fill the required field !!",
+      });
+    }
+
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+      return res.send({
+        status: false,
+        res: "User with this email id does not exist !!",
+      });
+    }
+
+    var Password = bcrypt.compareSync(password, userExist?.password);
+
+    res.send({
+      status: true,
+      res: "User LogIn Successfully !!",
+      userExist,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = {
   createUser,
+  signIn,
 };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controllers/auth.controller");
+
+const { createUser, signIn } = require("../controllers/auth.controller");
 const router = express.Router();
 
-router.post("/create-user", createUser);
+router.post("/sign-up", createUser);
+router.post("/sign-in", signIn);
 
 module.exports = router;
